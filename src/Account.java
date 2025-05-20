80cfff17533edde594e036743ee8b1cf5868cc90
Parthiangold <nathfann01@gmail.com>
2025-05-20 14:22:58 +1000

    Implement Account class with MyFileIO interface support
    Added the `Account` class with fields, constructors, getters/setters, and methods to read and write data using the `MyFileIO` interface. This provides functionality for managing account information and ensures consistent input/output operations.

--- src/Account.java
+++ src/Account.java
@@ -1,2 +1,66 @@
-public class Account {
+import java.util.Scanner;
+
+public class Account implements MyFileIO{
+    private int number;
+    private String bank;
+    private double balance;
+    private String type;
+
+    //Empty Constructor Class
+    public Account(){}
+
+    //Parameterised Constructor
+    public Account(int number, String bank, double balance, String type){
+        this.number = number;
+        this.bank = bank;
+        this.balance = balance;
+        this.type = type;
+    }
+
+    //Getters and Setters
+    public int getNumber() {
+        return number;
+    }
+    public void setNumber(int number) {
+        this.number = number;
+    }
+    public String getBank() {
+        return bank;
+    }
+    public void setBank(String bank) {
+        this.bank = bank;
+    }
+    public double getBalance() {
+        return balance;
+    }
+    public void setBalance(double balance) {
+    }
+    public String getType() {
+        return type;
+    }
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    // Read Data Method
+    @Override
+    public void readData(Scanner sc){
+        this.number = sc.nextInt();
+        this.bank = sc.next();
+        this.balance = sc.nextDouble();
+        this.type = sc.next();
+    }
+
+    // Write Data Method
+    @Override
+    public void writeData(java.util.Formatter f){
+        f.format("%d %s %.2f %s", this.number, this.bank, this.balance, this.type);
+    }
+
+    // toString Method
+    @Override
+    public String toString(){
+        return String.format("%d %s %.2f %s", this.number, this.bank, this.balance, this.type);
+    }
+
 }
--- src/MyFileIO.java
+++ src/MyFileIO.java
@@ -6,6 +6,4 @@ public interface MyFileIO{
     void readData(Scanner sc);
     void writeData(Formatter f);
     String toString();
-
-
 }
