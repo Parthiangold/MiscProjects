0899b43e6aa7dae77f9d9aaa5f8d043602056b1b
Parthiangold <nathfann01@gmail.com>
2025-05-20 14:10:38 +1000

    Add MyFileIO interface with read, write, and toString methods
    Introduced a new interface `MyFileIO` to standardize file I/O operations. It includes methods for reading data, writing data, and converting objects to strings for improved consistency and functionality.

--- src/MyFileIO.java
+++ src/MyFileIO.java
@@ -1,2 +1,11 @@
+import java.util.Scanner;
+import java.util.Formatter;
+
+// Interface Added and implemented
 public interface MyFileIO{
+    void readData(Scanner sc);
+    void writeData(Formatter f);
+    String toString();
+
+
 }
