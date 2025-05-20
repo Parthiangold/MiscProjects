fb9ca9c222bea2c5481f9b61b3181e8198cb0ef0
Parthiangold <nathfann01@gmail.com>
2025-05-20 14:23:44 +1000

    Add bank and balance fields with getters, setters, and equals
    Introduced `bank` and `balance` fields along with their respective getters and setters to manage account data. Overrode the `equals` method to allow comparison of `bank` names with case insensitivity.

--- src/BankBalance.java
+++ src/BankBalance.java
@@ -1,2 +1,33 @@
 public class BankBalance {
+    private String bank;
+    private double balance;
+
+    public BankBalance(String bank, double balance) {
+        this.bank = bank;
+        this.balance = balance;
+    }
+
+    public String getBank() {
+        return bank;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBank(String bank) {
+        this.bank = bank;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof String) {
+            return bank.equalsIgnoreCase((String) obj);
+        }
+        return false;
+    }
 }
